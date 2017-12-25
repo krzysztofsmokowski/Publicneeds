7cb7c0b5ed439185eb84caf2a100b435561b7dac
Python School <school@python.com>
2017-12-25 12:43:39 +0100

    fixed according to pull request

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
-import requests
+from flask import Flask, json
+from flask_restful import reqparse, abort, Api, Resource
+from publicneeds import PublicNeeds
 
-class PublicNeeds(object):
-    def __init__(self):
-        pass
+app = Flask(__name__)
+api = Api(app)
 
-    def requesting(self, page=1, limit=1000, last_page=4):
-        '''
-        Kwarg page will be used in for to iterate from this number to last_page
-        Limit is also used with default value of 100 as recommended in api docs.
-        For now i willl use self.requested_pages to append
-        '''
-        requested_pages = []
-        for dictionary in range(page, last_page):
-            req = requests.get('https://api-v3.mojepanstwo.pl/dane/zamowienia_publiczne.json?page={}&limit={}'.format(page, limit)).json()
-            page+=1
-            requested_pages.append(req)
-            return(requested_pages)
+class Region(Resource):
+    def get(self, region):
+        pubs = PublicNeeds()
+        data = pubs.service_information(region=region)
+        return data, 200
 
-    def service_information(self, region):
-        requested_jsons = self.requesting()
-        without_executor = []
-        output_dict = {}
-        for single_json in requested_jsons:
-            for orders in single_json["Dataobject"]:
-                if orders["data"]["zamowienia_publiczne.zamawiajacy_wojewodztwo"] == region:
-                    output_dict[orders["data"]["zamowienia_publiczne.nazwa"]] = {'rodzaj robot': orders["data"]["zamowienia_publiczne_rodzaje.nazwa"], 'wykonawca' : orders["data"]["zamowienia_publiczne.wykonawca_str"]}
-        return output_dict
 
-def main():
-    print('done')
-    pubs = PublicNeeds()
-
+api.add_resource(Region, '/region/<region>')
 if __name__ == '__main__':
-    main()
+    app.run(debug=False, host='0.0.0.0', port=8000)
